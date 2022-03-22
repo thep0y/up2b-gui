816fc9f5dad97a3e9502719436444a2d57b7e831
thepoy <thepoy@163.com>
2022-03-22 19:59:30 +0800

    修复linux中因部分文件权限过高导致无法正常启动的问题

--- pkg/up2b/opt/up2b/run.sh
+++ pkg/up2b/opt/up2b/run.sh
@@ -1,4 +1,12 @@
-#/bin/sh
+#!/bin/sh
 
-install_dir=/opt/up2b
-$install_dir/venv/bin/python $install_dir/main.py
+venv_dir=$HOME/.config/up2b/venv
+
+if [ ! -d $venv_dir ]; then
+    python3 -m venv $venv_dir
+    # 不升级 pip 无法安装 pyqt5
+    $venv_dir/bin/python -m pip install pip --upgrade -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+    $venv_dir/bin/python -m pip install 'pywebview[qt]' up2b 'flask[async]' -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+fi
+
+$venv_dir/bin/python /opt/up2b/main.py
